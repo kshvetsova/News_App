5a789a8691b170536e621fff19e94a7e6692bef1
kshvetsova <katshvetsova@gmail.com>
2021-07-01 02:50:24 +0300

    Updates

---
+++ precache-manifest.9a67ce550c2d795dc440744ef0a3b7ce.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5054b53d30a00514185b87f77808dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44f22327bc23e9085085",
+    "url": "./static/css/main.5f43f06e.chunk.css"
+  },
+  {
+    "revision": "e5a6d6e97912bd38ff52",
+    "url": "./static/js/2.556eb0bf.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.556eb0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f22327bc23e9085085",
+    "url": "./static/js/main.02241b05.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  },
+  {
+    "revision": "a594ab6047fb39e46b538aa2f6e8f160",
+    "url": "./static/media/cross.a594ab60.svg"
+  },
+  {
+    "revision": "45aed7e599ab6004487f4bd3981001cc",
+    "url": "./static/media/input-search.45aed7e5.svg"
+  },
+  {
+    "revision": "d64e3f84c5f4bb7c19d5bb85540f51fd",
+    "url": "./static/media/instagram-hover.d64e3f84.svg"
+  },
+  {
+    "revision": "9904aa8862411ef854fc109e1cb00df1",
+    "url": "./static/media/instagram.9904aa88.svg"
+  },
+  {
+    "revision": "6f076677148b9b98ec60d155f0aacd7c",
+    "url": "./static/media/linkedin-hover.6f076677.svg"
+  },
+  {
+    "revision": "b6fbb2ef057d515e868b1a0bbaddab3d",
+    "url": "./static/media/linkedin.b6fbb2ef.svg"
+  },
+  {
+    "revision": "72b6e79467b03a8f9f0812fe5b818a8f",
+    "url": "./static/media/menu.72b6e794.svg"
+  },
+  {
+    "revision": "9c68d8ef50deeb6da8bfac46a7a50ada",
+    "url": "./static/media/scroll-articles.9c68d8ef.svg"
+  },
+  {
+    "revision": "b15f14bae267df45f9304d96ec9de331",
+    "url": "./static/media/scroll-small.b15f14ba.svg"
+  },
+  {
+    "revision": "71085b258ca15739dea54c5dccf86bba",
+    "url": "./static/media/scroll.71085b25.svg"
+  },
+  {
+    "revision": "17c37846df9743859d7e3849a7bd790b",
+    "url": "./static/media/twitter-hover.17c37846.svg"
+  },
+  {
+    "revision": "294e4a90384208962d705e6e4ad7e63d",
+    "url": "./static/media/twitter.294e4a90.svg"
+  }
+]);
