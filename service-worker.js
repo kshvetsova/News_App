dccb24caaaeff4ea7ceea51e68be9668db59bcec
kshvetsova <katshvetsova@gmail.com>
2021-07-12 13:37:57 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a313c468c3fccabfaac56816d2d3e24a.js"
+  "./precache-manifest.7ad2d1b1d4fb42f0052d53d77b592c0e.js"
 );
 
 self.addEventListener('message', (event) => {
